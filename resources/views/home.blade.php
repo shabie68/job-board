36ad7c7d9e28bc490f792e06fea3b9296666f7cf
Shabie Ul Hassan <shabeeulhassan40@gmail.com>
2024-03-02 11:29:20 +0500

    configure react

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div>
+                <div>{{ __('Dashboard') }}</div>
+
+                <div>
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <div id="app">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
